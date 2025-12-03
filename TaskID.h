70c8a3a01ffaec9878d8ddb1fdc5071eb3e48840
Ben Runswick <ben.runswick@gmail.com>
2025-12-03 14:00:55 +0000

    Enhance TaskID class with owner and serialization
    Added ownerUsername attribute and methods for serialization and deserialization.

--- TaskID.h
+++ TaskID.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -20,8 +21,8 @@ public:
     int priorityHigh;
     int priorityMid;
     int priorityLow;
+    string ownerUsername;
 
-    // Default Constructor
     TaskID() {
         taskID = 0;
         title = "";
@@ -31,9 +32,9 @@ public:
         dueDayDate = dueMonthDate = dueYearDate = 0;
         priority = "";
         priorityHigh = priorityMid = priorityLow = 0;
+        ownerUsername = "";
     }
 
-    // Setters
     void setTitle(string t) { title = t; }
     void setDescription(string d) { description = d; }
     void setStatus(int s) { status = s; }
@@ -41,6 +42,7 @@ public:
     void setDueDay(int d) { dueDayDate = d; }
     void setDueMonth(int m) { dueMonthDate = m; }
     void setDueYear(int y) { dueYearDate = y; }
+    void setOwner(string owner) { ownerUsername = owner; }
     void setPriority(string pr, int pH, int pM, int pL) {
         priority = pr;
         priorityHigh = pH;
@@ -48,7 +50,6 @@ public:
         priorityLow = pL;
     }
 
-    // Getters
     string getTitle() { return title; }
     string getDescription() { return description; }
     int getStatus() { return status; }
@@ -57,16 +58,66 @@ public:
     int getDueMonth() { return dueMonthDate; }
     int getDueYear() { return dueYearDate; }
     string getPriority() { return priority; }
+    string getOwner() { return ownerUsername; }
+
+    void serialize(ofstream& out) {
+        out << taskID << endl;
+        out << title << endl;
+        out << description << endl;
+        out << status << endl;
+        out << assignedTo << endl;
+        out << dueDayDate << endl;
+        out << dueMonthDate << endl;
+        out << dueYearDate << endl;
+        out << priority << endl;
+        out << priorityHigh << endl;
+        out << priorityMid << endl;
+        out << priorityLow << endl;
+        out << ownerUsername << endl;
+    }
+
+    void deserialize(ifstream& in) {
+        in >> taskID;
+        in.ignore();
+        getline(in, title);
+        getline(in, description);
+        in >> status;
+        in.ignore();
+        getline(in, assignedTo);
+        in >> dueDayDate >> dueMonthDate >> dueYearDate;
+        in.ignore();
+        getline(in, priority);
+        in >> priorityHigh >> priorityMid >> priorityLow;
+        in.ignore();
+        getline(in, ownerUsername);
+    }
 };
 
-// Operator to print a Task
-ostream& operator<<(ostream& os, const TaskID& t) {
-    os << "\n----- Task Details -----\n";
-    os << "Task ID: " << t.taskID << endl;
-    os << "Title: " << t.title << endl;
-    os << "Priority: " << t.priority << endl;
-    os << "-------------------------\n";
+inline ostream& operator<<(ostream& os, const TaskID& t) {
+    string statusText;
+    switch (t.status) {
+        case 1: statusText = "Pending"; break;
+        case 2: statusText = "In Progress"; break;
+        case 3: statusText = "Completed"; break;
+        default: statusText = "Unknown"; break;
+    }
+
+    os << "\n========================================\n";
+    os << "              TASK DETAILS              \n";
+    os << "========================================\n";
+
+    os << "Task ID     : " << t.taskID << '\n';
+    os << "Title       : " << t.title << '\n';
+    os << "Description : " << t.description << '\n';
+    os << "Assigned To : " << t.assignedTo << '\n';
+    os << "Status      : " << statusText << '\n';
+    os << "Due Date    : " << t.dueDayDate << "/"
+       << t.dueMonthDate << "/" << t.dueYearDate << '\n';
+    os << "Priority    : " << t.priority << '\n';
+    os << "Owner       : " << t.ownerUsername << '\n';
+
+    os << "----------------------------------------\n";
     return os;
 }
 
-#endif 
+#endif
