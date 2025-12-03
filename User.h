a6ade2f1fb29b6014c8bb8ae08b37527874202cc
Ben Runswick <ben.runswick@gmail.com>
2025-12-03 14:24:39 +0000

    Refactor User class with comments and formatting
    Updated User class with comments and improved structure.

--- User.h
+++ User.h
@@ -6,44 +6,56 @@
 #include <string>
 using namespace std;
 
+// User class stores login details for a single user
 class User {
 private:
     string username;
     string password;
     
 public:
+    // Default constructor
     User() : username(""), password("") {}
     
+    // Overloaded constructor
     User(string n, string p){
         username = n;
         password = p;
     }
 
+    // Get username
     string getUsername(){return username;}
+    // Get password
     string getPassword(){return password;}
 
+    // Set username
     void setUsername(string n){username = n;}
+    // Set password
     void setPassword(string p){password = p;}
 
+    // Save this user to file
     void serialize(ofstream& out) {
         out << username << endl;
         out << password << endl;
     }
 
+    // Load this user from file
     void deserialize(ifstream& in) {
         getline(in, username);
         getline(in, password);
     }
 
+    // Example account creation message (not used for logic)
     void createAccount(){
         cout << "Creating account for " << username << endl;
     }
 
+    // Check login credentials
     bool login(string n, string p){
         return (n == username && p == password);
     }
 };
 
+// Save all users to users.txt
 inline void saveUsers(User** users, int userCount) {
     ofstream outFile("users.txt");
     if (!outFile) {
@@ -58,6 +70,7 @@ inline void saveUsers(User** users, int userCount) {
     outFile.close();
 }
 
+// Load all users from users.txt
 inline int loadUsers(User** users) {
     ifstream inFile("users.txt");
     if (!inFile) {
